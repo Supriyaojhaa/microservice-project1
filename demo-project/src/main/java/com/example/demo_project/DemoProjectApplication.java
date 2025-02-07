e2657f0a08d79b9911bd16c9c5d8552d2c322f5e
Supriyaojhaa <supriyasojha@gmail.com>
2025-02-07 11:20:38 +0530

    second commit after 2 changes in controller and bean

--- demo-project/src/main/java/com/example/demo_project/DemoProjectApplication.java
+++ demo-project/src/main/java/com/example/demo_project/DemoProjectApplication.java
@@ -2,12 +2,20 @@ package com.example.demo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @SpringBootApplication
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
-		System.out.println("First Spring Project");
+		//System.out.println("First Spring Project");
 	}
 }
